964ac4229c72afa4a95b946b6fca169976989ed1
Maëlle Akpweh <117738735+maelleakp@users.noreply.github.com>
2023-01-25 22:09:53 +0100

    Add files via upload

---
+++ THOR/THOR.DataAccess/BaseDeDonnees/DIFFBD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace THOR.DataAccess.BaseDeDonnees
+{
+	public class DIFFBD : DbContext
+	{
+        public DIFFBD()
+        {
+        }
+        public DIFFBD (DbContextOptions<DIFFBD> options):base(options)
+		{
+			
+		}
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            IConfigurationRoot _configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server = 127.0.0.1, 1433; Database = test; user id = sa; password = MyPass@word; TrustServerCertificate = true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        
+    }
+}
+
